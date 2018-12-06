41951153610aca631adbc04159d386d52eb8a438
Daan Henke <daan@daan.vodka>
2018-12-06 01:12:31 +0100

    Added comments to main function

--- FreeRune.Server/Program.cs
+++ FreeRune.Server/Program.cs
@@ -9,12 +9,17 @@ namespace FreeRune.Server
     {
         static void Main(string[] args)
         {
+            // Set the log level to verbose, we're still in development so this is a semi sane default
             Logger.CurrentLogLevel = LogLevel.VERBOSE;
             
+            // Create & run an async game server
             GameServer gameServer = new GameServer();
             gameServer.Port = 43594;
             gameServer.Listen();
+            
+            // TODO: Implement a JAGGRAB server
 
+            // Sleep until all servers are done executing
             while (gameServer.IsRunning)
             {
                 Thread.Sleep(100);
